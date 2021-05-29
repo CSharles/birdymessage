5873aaf95cad0285d98c98b7f99527250d9186b2
Carlos Garcia <carlossgarcia@outlook.com>
2021-05-28 23:24:37 -0600

    Register secction completed

---
+++ src/components/Register.vue
@@ -0,0 +1,101 @@
+<template>
+<div class="card">
+      <font-awesome-icon icon="twitter" size="lg" fixed-width />
+      <h2>Create your account</h2>
+      <div class="register" v-if="!registered">
+        <form v-on:submit.prevent="registerAccount()">
+          <div class="form_group">
+            <label for="name">Name</label>
+            <span>{{name.length +'/'+max_length}}</span>
+            <input type="text" v-model="name" :maxlength="max_length" required>
+          </div>
+          <div class="form_group">
+            <label for="email">Email</label>
+            <span>{{email.length +'/'+max_length}}</span>
+            <input type="email" v-model="email" :maxlength="max_length" required>
+          </div>
+          <div class="form_group">
+            <label for="password">Password</label>
+            <span>{{password.length +'/'+max_pass_length}}</span>
+            <input type="email" v-model="password" :maxlength="max_pass_length" required>
+          </div>
+          <button type="submit">Register</button>
+        </form>
+        <div v-if="error.length > 0">{{ error }}</div>
+      </div>
+
+      <div class= "tweetBox" v-else>
+        <h2>Welcome {{userData.name}} write your first Tweet</h2>
+        Tweetbox</div>
+
+</div>
+
+</template>
+
+<script>
+export default {
+  name: 'Register',
+  data() {
+     return {
+      userData:{},
+      usersID: 0,
+      name:'',
+      email:'',
+      password:'',
+      max_length:25,
+      max_pass_length:16,
+      registered: false,
+      error:'',
+    }
+  },
+  methods:{
+    registerAccount(){
+      if(this.name !=='' && this.email !=='' && this.password !==''){
+        this.userData.id = ++this.usersID
+        this.userData.name= this.name
+        this.userData.email = this.email
+        this.userData.password = this.password
+      }
+      else{
+        this.error = 'Complet all the form fields'
+      }
+
+      localStorage.setItem('simple_tweet_registered',true)
+      localStorage.setItem('simple_tweet_registered_user', JSON.stringify(this.userData))
+
+      this.name=''
+      this.email=''
+      this.password=''
+
+    }
+  },
+  created(){
+    const registeredUser = localStorage.getItem('simple_tweet_registered_user')
+    const registered = localStorage.getItem('simple_tweet_registered')
+    if(registered === 'true') {
+      this.registered = true
+    }
+    if(registeredUser){
+      this.userData = JSON.parse(registeredUser)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
